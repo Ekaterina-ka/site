59e5c2090ae056ae98b6d0e945ae3f58ff6be04e
Kate <martynovakata98908@gmail.com>
2025-05-30 15:43:41 +0300

    message

---
+++ jss.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu')
+menu = '.menu'
+menu.addEventListener('.click', function(){
+  window.location.href = ('http://localhost:63342/untitledTest/menu.html?_ijt=p58r5bqntpuk941itlfphbjir3&_ij_reload=RELOAD_ON_SAVE')
+
+})
+document.getElementById('price').addEventListener('onclick', function(){
+  window.location.href = 'https://img.freepik.com/free-vector/wedding-planning-template-design_52683-134692.jpg?t=st=1745956168~exp=1745959768~hmac=79f8435841165211803da898287edbd5127f1025944d550334059f914631d6b6&w=740';
+
+})
+
+document.querySelector('.price')
+price='.price'
+price.onclick = function() {
+  location.href = ''
+}
+
+gsap.to(".title", {});
+
